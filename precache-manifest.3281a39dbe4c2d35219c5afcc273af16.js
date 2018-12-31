b3aeed468f6a4a95454b2945576941bfe101b353
surya <surya.shandil@ck12.org>
2018-12-31 13:13:54 +0530

    Updates

---
+++ precache-manifest.3281a39dbe4c2d35219c5afcc273af16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b91ee5316870eb0594aa",
+    "url": "/paintings/static/js/runtime~main.b91ee531.js"
+  },
+  {
+    "revision": "7972b284a4ec4c5a4d75",
+    "url": "/paintings/static/js/main.7972b284.chunk.js"
+  },
+  {
+    "revision": "17934efebcda40ef180a",
+    "url": "/paintings/static/js/1.17934efe.chunk.js"
+  },
+  {
+    "revision": "7972b284a4ec4c5a4d75",
+    "url": "/paintings/static/css/main.085e1066.chunk.css"
+  },
+  {
+    "revision": "b538e78aec7775b8c604d3d177595996",
+    "url": "/paintings/index.html"
+  }
+];
