f2130fc89faf03d649ab4ab255fcee844bdda06e
surya <surya.shandil@ck12.org>
2018-12-31 12:53:53 +0530

    some more config

---
+++ docs/precache-manifest.d47de0c42309aed35fc574894ff7584d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "81fea9c48982b01dda24",
+    "url": "/static/js/main.81fea9c4.chunk.js"
+  },
+  {
+    "revision": "17934efebcda40ef180a",
+    "url": "/static/js/1.17934efe.chunk.js"
+  },
+  {
+    "revision": "81fea9c48982b01dda24",
+    "url": "/static/css/main.085e1066.chunk.css"
+  },
+  {
+    "revision": "ff5e548c7cf08922a250219f0ae63197",
+    "url": "/index.html"
+  }
+];
