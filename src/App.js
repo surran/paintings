af5f173780701f7780ab8183a8a9b75b55034396
Surya Ranjan Shandil <suryaran@gmail.com>
2019-10-22 11:22:25 +0530

    mobile font fix

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ class App extends Component {
 
     return (
       <Router  basename={process.env.PUBLIC_URL}>
-      <div style={{backgroundColor:"#fbfbfb",  margin: 0, padding: 0, fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif'
+      <div style={{backgroundColor:"#fbfbfb",  margin: 0, padding: 0, fontFamily: '"Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif'
   , WebkitFontSmoothing: "antialiased", MozOsxFontSmoothing: "grayscale", lineHeight:"1.6"}} className="App">
         
         {/* Header */}
